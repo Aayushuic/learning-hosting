70d9cc5c7377252d8e60388c8029874ee7fcbbfa
walkwel-dev <wdev969@gmail.com>
2025-05-11 17:35:47 +0530

    databaase2

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -4,7 +4,7 @@ import path from 'path';
 export const AppDataSource = new DataSource({
   type: 'postgres',
   // host: process.env.DB_HOST || 'localhost',
-  url: 'postgresql://happydot_user:xf8B0T2vAGPDYRU7CpbqOQF2LgfD5Gry@dpg-d0g8cgi4d50c73fit4i0-a.oregon-postgres.render.com/happydot',
+  url: process.env.DB_URL,
   // port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
   // username: process.env.DB_USERNAME || 'postgres',
   // password: process.env.DB_PASS,
