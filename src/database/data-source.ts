3c992fce3203c08116d94a86b74285163f80075a
walkwel-dev <wdev969@gmail.com>
2025-05-11 18:08:24 +0530

    helllo

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,20 +1,30 @@
 import { DataSource } from 'typeorm';
 import path from 'path';
+import { User } from '../entities/user.entity';
+import { Campaign } from '../entities/campaign.entity';
+import { CampaignRule } from '../entities/campaign-rule.entity';
+import { Referral } from '../entities/referral.entity';
+import { Role } from '../entities/role.entity';
+import { Permission } from '../entities/permission.entity';
+import { Otp } from '../entities/otps.entity';
+import { RewardProvider } from '../entities/reward-provider.entity';
+import { Rewards } from '../entities/reward.entity';
+import { UserMetaData } from '../entities/user-meta-data.entity';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   // host: process.env.DB_HOST || 'localhost',
-  url: process.env.DB_URL,
+  url: 'postgresql://happydot_user:xf8B0T2vAGPDYRU7CpbqOQF2LgfD5Gry@dpg-d0g8cgi4d50c73fit4i0-a.oregon-postgres.render.com/happydot',
   // port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
   // username: process.env.DB_USERNAME || 'postgres',
   // password: process.env.DB_PASS,
   // database: process.env.DB_NAME || 'happyDot',
-  synchronize: process.env.ENV === 'Development' ? true : false,
+  synchronize: true,
   logging: false,
   ssl: {
     rejectUnauthorized: false, // This is required for Render PostgreSQL
   },
-  entities: [__dirname, 'src/entities/**/*.{ts,js}'],
+  entities: [User,Campaign,CampaignRule,Referral,Role,Permission,Otp,RewardProvider,Rewards,UserMetaData],
   migrations: [__dirname, '/migrations/**/*.{ts,js}'],
   subscribers: [],
 });
